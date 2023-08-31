778830a04f8c9d8ac8fd91d07d11b721bf87c231
Gabriel Kakuhama Ribeiro <gabriel.kakuhama@aluno.senai.br>
2023-08-31 10:24:56 -0300

    Criando interfaces para validar nome e emaildo cliente

--- Fundamentos/src/orientacaoobjetos/Cliente.java
+++ Fundamentos/src/orientacaoobjetos/Cliente.java
@@ -1,17 +1,18 @@
 package orientacaoobjetos;
 
 public class Cliente {
-private String nome;
-private String email;
+
+    private String nome;
+    private String email;
 
     public Cliente(String nome, String email) {
         this.nome = nome;
         this.email = email;
     }
 
-    public Cliente (String nome ){
+    public Cliente(String nome) {
         this.nome = nome;
-    } 
+    }
 
     public String getNome() {
         return nome;
@@ -23,7 +24,28 @@ private String email;
 
     @Override
     public String toString() {
-        return "Cliente{" + "nome=" + nome + ", email=" + email + '}';
+        if (this.email == null) {
+            return "Cliente{" + "nome=" + nome + '}';
+        } else {
+            return "Cliente{" + "nome " + nome + ", email=" + email + '}';
+        }
+
     }
+            /**
+             * Calcular o valor total da locação de filmes.
+             * Regra para todos os filmes  comuns.
+             * @param qtdeDias
+             * @return 
+             */
     
+    
+                public double calcularLocacao(int qtdeDias){
+                    return qtdeDias * 3.99;
+                }
+               
+                public double calcularLocacao(int qtdeDias, boolean lancamento){
+                    return qtdeDias * VALOR_FILME_LANCAMENTO;
+                }
+             
+               
 }
--- Fundamentos/src/orientacaoobjetos/Locadora.java
+++ Fundamentos/src/orientacaoobjetos/Locadora.java
@@ -9,8 +9,12 @@ public class Locadora {
         
         Cliente c1 = new Cliente("José Antonio dos Santos");
                 System.out.println(c1);
-                
+           double valorLocacaoSimples = c1.calcularLocacao(3);
+           System.out.println("O valor para locação do filme" + "simples é:" + valorLocacaoSimples);
+           
         Cliente c2 = new Cliente("Marilza Sílva", "marilza.silva@gmail.com");
                 System.out.println(c2);
+                double valorLocacaoLancamento = c2.calcularLocacao(03, true);
+                        System.out.println("O valor da locação do filme" + "lançamento é:" + valorLocacaoLancamento);
     }
 }
